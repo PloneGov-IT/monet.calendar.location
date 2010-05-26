08d11317da245b668d15287396e62513361c4108
redturtle <redturtle@6a6e2cd2-7798-44f2-a3d3-4feb5ca87cd5>
2010-05-26 10:43:39 +0000

    first commit location product

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1.0'
+
+setup(name='monet.calendar.location',
+      version=version,
+      description="Add Italian features to monet.calendar.event",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.net',
+      url='http://plone.comune.modena.it/svn/monet/monet.calendar.location',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['monet', 'monet.calendar'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
